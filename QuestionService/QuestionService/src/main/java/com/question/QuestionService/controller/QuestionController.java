0c2446bdece7d708e8f27e4c1fdeb9d038c6ef2c
dev_nick <developernikhil28@gmail.com>
2025-08-24 13:28:19 +0530

    Add controller

---
+++ QuestionService/QuestionService/src/main/java/com/question/QuestionService/controller/QuestionController.java
@@ -0,0 +1,42 @@
+package com.question.QuestionService.controller;
+
+
+import com.question.QuestionService.entity.Question;
+import com.question.QuestionService.service.QuestionService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/question")
+public class QuestionController {
+
+    private QuestionService questionService;
+
+    public QuestionController(QuestionService questionService) {
+        this.questionService = questionService;
+    }
+
+    // create
+    @PostMapping
+    public Question create(@RequestBody Question question){
+        return questionService.create(question);
+    }
+
+    // get all
+    @GetMapping
+    public List<Question> getAll(){
+        return questionService.get();
+    }
+
+    @GetMapping("/{questionId}")
+    public Question getOne(@PathVariable Long questionId){
+        return questionService.getOne(questionId);
+    }
+
+    // get all of specific quiz
+    @GetMapping("/quiz/{quizId}")
+    public List<Question> getQuestionsOfQuiz(@PathVariable Long quizId){
+        return questionService.getQuestionsOfQuiz(quizId);
+    }
+}
